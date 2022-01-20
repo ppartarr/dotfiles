bbe418c0618326b2e4c89379591c231d88f23e6e
Philippe Partarrieu <ppartarrieu@minimaxlabs.com>
2022-01-20 08:27:39 +0100

    merge

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@
 git submodule init
 git submodule update
 
-# what directories should be installable by all users including the root user
-base=(
-)
 
 # folders that should, or only need to be installed for a local user
 useronly=(
@@ -31,11 +28,6 @@ stowit() {
 
 echo -e "Stowing apps for user: $USER\n"
 
-# install apps available to local users and root
-for app in ${base[@]}; do
-    stowit "${HOME}" $app
-done
-
 # install only user space folders
 for app in ${useronly[@]}; do
     if [[ ! "$USER" = *"root"* ]]; then
@@ -46,3 +38,5 @@ done
 if [[ "${useronly[@]}" =~ "${xorg}" ]]; then
   cp -R .xcolors ~/.xcolors
 fi
+
+cp -r ./bin ~/bin/
